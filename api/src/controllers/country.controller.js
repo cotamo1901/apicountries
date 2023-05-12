414383d014337508f8a0332ed8ec787dc1803066
John Cotamo <johncotamo@hotmail.com>
2023-05-11 21:46:40 -0500

    add modification

--- api/src/controllers/country.controller.js
+++ api/src/controllers/country.controller.js
@@ -1,9 +1,14 @@
+const { pokemon } = require("../db");
+const URL_API = process.env.URL_API;
 
-const {pokemon} = require('../db')
-const URL_API = process.env.URL_API
-
-async function getCountryApi(){
-    const response = await fetch(URL_API)
-    let data = await response.json()
-    data = data.map(())
+async function getCountryApi() {
+  const response = await fetch(URL_API);
+  let data = await response.json();
+  data = data.map((country) => ({
+    id: country.id,
+  }));
 }
+
+module.exports = {
+  getCountryApi,
+};
