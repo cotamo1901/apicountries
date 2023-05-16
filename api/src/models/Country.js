adc055429371f6fe1387315285180af507992c71
John Cotamo <johncotamo@hotmail.com>
2023-05-15 20:44:39 -0500

    se modifica  modelos

--- api/src/models/Country.js
+++ api/src/models/Country.js
@@ -1,38 +1,38 @@
 const { DataTypes, Sequelize } = require("sequelize");
-module.export = (sequelize) => {
+
+module.exports = (sequelize) => {
   sequelize.define("country", {
-    id: {
-      type: DataTypes.UUID,
+   id:{
+    type: DataTypes.UUID,
       defaultValue: DataTypes.UUIDV4,
       allowNull: false,
       primaryKey: true,
-    },
-    name: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    flagImage: {
-      type: DataTypes.BLOB,
-      allowNull: false,
-    },
-
-    continent: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    capital: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    subregion: {
-      type: DataTypes.STRING,
-    },
-    area: {
-      type: DataTypes.FLOAT,
-    },
-    population: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-    },
+   },
+   name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  flagImage: {
+    type: DataTypes.BLOB,
+    allowNull: false,
+  },
+  continent: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  capital: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  subregion: {
+    type: DataTypes.STRING,
+  },
+  area: {
+    type: DataTypes.FLOAT,
+  },
+  population: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
   });
 };
